1c4c6ff8f8fffe82093152471690cc5288cb2202
Menaili Haroun Errachide <menailimic@gmail.com>
2025-10-03 20:56:16 +0100

    Update RhApplication.java

--- src/main/java/dz/touche/rh/RhApplication.java
+++ src/main/java/dz/touche/rh/RhApplication.java
@@ -2,7 +2,7 @@ package dz.touche.rh;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//testt
 @SpringBootApplication
 public class RhApplication {
 
